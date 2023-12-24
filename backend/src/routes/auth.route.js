9d025eabf8a5878c7c960254b3afb8e71225c8a7
Manish <mdashsharma95@gmail.com>
2023-12-24 12:07:33 +0530

    Restructured Repository

--- backend/src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getProfile } from '../controllers/auth.controller.js';
-
-const router = Router()
-
-router.post("/test-decode-encode", getProfile)
-
-
-export default router
