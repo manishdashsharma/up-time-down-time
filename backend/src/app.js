e38c4ff65fde22930a1d1424d80e16c68cc478f5
Manish <mdashsharma95@gmail.com>
2023-12-26 18:05:21 +0530

    Setup documentation route

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,11 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import routes from "./routes/index.js"
+import swaggerUi from 'swagger-ui-express'
+import { readFileSync } from 'fs';
+
+const filePath = new URL('../swagger.json', import.meta.url);
+const swaggerDocument = JSON.parse(readFileSync(filePath));
 
 const app = express()
 
@@ -10,14 +15,11 @@ app.use(express.urlencoded({extended: true}))
 app.use(cors())
 app.use(cookieParser())
 
-app.use("/api/v1/", routes)
 
-app.get("/", (_req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "If you are seeing this then, server is !down"
-    })
-})
+app.use("", routes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 
 app.all("*", (_req, res) => {
     return res.status(404).json({
---
+++ backend/swagger.js
@@ -0,0 +1,33 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+    info: {
+        title : "Up Time Down Time Documentation",
+        description: "Up-Time-Down-Time is a comprehensive project designed to monitor website uptime and downtime"
+    },
+    "servers": [
+        {
+          "url": "http://localhost:5000/",
+          "description": "Local server"
+        },
+        {
+          "url": "https://up-time-down-time-n6ky87t9d-manishdashsharma.vercel.app/",
+          "description": "Production server"
+        }
+      ],
+      "tags": [
+        {
+          "name": "Health Check",
+          "description": "Endpoints related to system health check"
+        },
+        {
+          "name": "Authentication",
+          "description": "Endpoints related to user authentication"
+        }
+      ],
+}
+
+const outputFile = './swagger.json'
+const route = ["./src/routes/index.js"]
+
+swaggerAutogen(outputFile, route,doc)
